1887d5bfd3db83ed12c02cbd6967f05abcd573b8
Chen-Abudi <cheezumcali23@gmail.com>
2022-10-13 19:15:41 +0300

    .env

--- utils/key.js
+++ utils/key.js
@@ -3,4 +3,6 @@ const crypto = require('crypto');
 // gonna move this when making .env !
 const secretKey = crypto.randomBytes(32).toString('hex');
 
+console.log(secretKey); // 86d04d0a6631295a2f75d163e662c5a7e4e531c4415c588b576962454280d4ff
+
 module.exports = { secretKey };
